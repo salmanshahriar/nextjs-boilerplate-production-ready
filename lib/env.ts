4a31592e4d83d8ed31c3e351613accf9b13cb6b5
salmanshahriar <salmanshahriar16@gmail.com>
2026-02-09 17:20:53 +0600

    chore: remove unnecessary whitespace in configuration files

--- lib/env.ts
+++ lib/env.ts
@@ -8,4 +8,3 @@ const envSchema = z.object({
 });
 
 export const env = envSchema.parse(process.env);
-
--- playwright.config.ts
+++ playwright.config.ts
@@ -21,4 +21,3 @@ export default defineConfig({
     reuseExistingServer: !process.env.CI,
   },
 });
-
